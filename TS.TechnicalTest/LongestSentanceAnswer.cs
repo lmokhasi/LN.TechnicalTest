f71153c1c288d1cd796b1736cb3b54fd5c09fccc
lmokhasi <65120253+lmokhasi@users.noreply.github.com>
2025-11-03 15:09:48 +0200

    LongestAnswerTest

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,19 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        char[] separators = { ',', '.', '!', '?' };
+        var words = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        var longestString = 0;
+
+        foreach (var item in words)
+        {
+            var arrayItem = item.Trim().Split(' ');
+            if (arrayItem.Length >= 0 && arrayItem.Length > longestString)
+            {
+                longestString = arrayItem.Length;
+            }
+
+        }
+        return longestString;
     }
 }
