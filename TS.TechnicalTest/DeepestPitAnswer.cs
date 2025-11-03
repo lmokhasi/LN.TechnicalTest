0792b1e44e0dcae220fab5a1b39c9c9490f2aa05
lmokhasi <65120253+lmokhasi@users.noreply.github.com>
2025-11-03 15:19:09 +0200

    DeeperPitAnswer

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -4,6 +4,47 @@ public class DeepestPitAnswer
 {
     public static int Solution(int[] points)
     {
-        throw new NotImplementedException("Not completed yet");
+        if (points == null || points.Length < 3)
+        {
+            return -1;
+        }
+
+        int N = points.Length;
+        int maxDepth = 0;
+        int p = 0;
+
+        while (p < N - 2)
+        {
+            int q = p + 1;
+            while (q < N - 1 && points[q - 1] > points[q])
+            {
+                q++;
+            }
+
+            if (q == p + 1)
+            {
+                p = q;
+                continue;
+            }
+
+            int r = q;
+            while (r < N && points[r - 1] < points[r])
+            {
+                r++;
+            }
+
+            if (r > q)
+            {
+                int currentDepth = Math.Min(points[p] - points[q - 1], points[r - 1] - points[q - 1]);
+                if (currentDepth > maxDepth)
+                {
+                    maxDepth = currentDepth;
+                }
+            }
+
+            p = r - 1;
+        }
+
+        return maxDepth > 0 ? maxDepth : -1;
     }
 }
--- TS.UnitTests/2. DeepestPitTests.cs
+++ TS.UnitTests/2. DeepestPitTests.cs
@@ -8,9 +8,18 @@ public class DeepestPitTests
     [TestMethod]
     public void Q2_BasicNominalTest()
     {
-        var points = new[] {0, 1, 3, -2, 0, 1, -3, 2, 3};
+        var points = new[] { 0, 1, 3, -2, 0, 1, -3, 2, 3 };
         var result = DeepestPitAnswer.Solution(points);
 
         Assert.AreEqual(4, result);
+    }
+
+    [TestMethod]
+    public void Q3_BasicNominalTest()
+    {
+        var points = new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+        var result = DeepestPitAnswer.Solution(points);
+
+        Assert.AreEqual(2, result);
     }
 }
